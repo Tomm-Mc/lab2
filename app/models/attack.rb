394947f8ced872c5c56bfac047c71f8a50ac9a70
Tomm-Mc <mcnallytomas@gmail.com>
2022-09-25 20:04:03 +0000

    Agregar Hirb

--- app/models/attack.rb
+++ app/models/attack.rb
@@ -1,2 +1,13 @@
 class  Attack < ApplicationRecord
+    belongs_to :monster
+    belongs_to :victim
+
+    validate :dif_edades
+
+    def dif_edades
+        if monster.scare_level > 5 && Date.today - victim.birthdate < 6574
+            errors.add("Esto es cualquiera")
+        end
+    end
+
 end
--- app/models/monster.rb
+++ app/models/monster.rb
@@ -1,4 +1,7 @@
 class Monster < ApplicationRecord
+    has_many :attacks
+    has_many :victims, through: :attacks
+
     validates :name, presence: true
 
     validates_uniqueness_of :name
